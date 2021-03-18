e96e10851336efe39209b98b4231ee78e437d35b
Anthony Agelvis <adrianagelvis_14@hotmail.com>
2021-03-18 10:19:30 -0400

    Modificacion o cambios dentro de la estructura

--- src/App.js
+++ src/App.js
@@ -2,6 +2,54 @@ import React from 'react';
 import "./styles/style.scss";
 import Cursos from "./Cursos";
 
+const cursos=[
+  {
+    title: "React desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    profesor: "Beto Quiroga",
+    price: 50,
+    imageTwo: "https://edteam-media.s3.amazonaws.com/courses/original/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg"
+  },
+  {
+    title: "Html desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/26557907-0555-427e-a40c-6ff207f98d72.png",
+    profesor: "Alvaro Felipe",
+    price: 30,
+    imageTwo: "https://edteam-media.s3.amazonaws.com/courses/original/26557907-0555-427e-a40c-6ff207f98d72.png"
+  },
+  {
+    title: "CSS desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/daa72e4e-c5d0-406e-9f6d-01e646bf719b.png",
+    profesor: "Alvaro Felipe",
+    price: 40,
+    imageTwo: "https://edteam-media.s3.amazonaws.com/courses/original/daa72e4e-c5d0-406e-9f6d-01e646bf719b.png"
+  },{
+    title: "Animaciones CSS desde cero",
+    image: "https://i.ytimg.com/vi/rbqVgBH0Wtg/maxresdefault.jpg",
+    profesor: "Alvaro Felipe",
+    price: 40,
+    imageTwo: "https://i.ytimg.com/vi/rbqVgBH0Wtg/maxresdefault.jpg"
+  },{
+    title: "Sass desde cero",
+    image: "https://i.ytimg.com/vi/3uFjh2ew86E/maxresdefault.jpg",
+    profesor: "Alvaro Felipe",
+    price: 40,
+    imageTwo: "https://i.ytimg.com/vi/3uFjh2ew86E/maxresdefault.jpg"
+  },{
+    title: "RWD desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/original/adf98e70-ade6-465e-b32c-2544b0bbded2.png",
+    profesor: "Alvaro Felipe",
+    price: 20,
+    imageTwo: "https://edteam-media.s3.amazonaws.com/courses/original/adf98e70-ade6-465e-b32c-2544b0bbded2.png"
+  },{
+    title: "Javascript desde cero",
+    image: "https://i.ytimg.com/vi/j9oy_rMRfAc/maxresdefault.jpg",
+    profesor: "Alvaro Felipe",
+    price: 20,
+    imageTwo: "https://i.ytimg.com/vi/j9oy_rMRfAc/maxresdefault.jpg"
+  },
+  
+]
 const App=()=> (
   <>
     <div className="main-banner img-container l-section" id="main-banner">
@@ -17,7 +65,11 @@ const App=()=> (
   </div>
 </div>
   <div className="ed-grid m-grid-3">
-    <Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos /><Cursos />
+    {
+      cursos.map(curso=> <Cursos title={curso.title} image={curso.image} profesor={curso.profesor}
+        imageTwo={curso.imageTwo} price={curso.price}
+      />)
+    }
   </div>
   </>
 )
--- src/Cursos.jsx
+++ src/Cursos.jsx
@@ -1,28 +1,74 @@
 import React from "react";
-
-const Cursos=()=>(
+import PropTypes from "prop-types";
+// const mayorEdad=(edad)=> edad > 18
+// const personas={"nombre": "Anthony", "apellido": "Agelvis", "edad": 20}
+// const curso={
+//   "title": "React desde cero",
+//   "image": "https://miro.medium.com/max/3840/0*oZLL-N4dGNlBe4Oh.png",
+//   "profesor": 'Beto Quiroga',
+//   "price": "50usd"
+// }
+// const Cursos=(props)=>(
+//     <article className="card">
+//     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+//       <img src={`${props.image}`} alt={props.title}/>
+//     </div>
+//     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+//         <h3></h3>
+     
+//       <div className="s-mb-2 s-main-center">
+//         <div className="card__teacher s-cross-center">
+//           <div className="card__avatar s-mr-1">
+//             <div className="circle img-container">
+//               <img src="https://images.pexels.com/photos/1105766/pexels-photo-1105766.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+//             </div>
+//           </div>
+//           <span className="small">{props.profesor}</span>
+//         </div>
+//       </div>
+//       <div className="s-main-center">
+//         <a className="button--ghost-alert button--tiny" href="#">{`$ ${props.price}`}</a>
+//       </div>
+//     </div>
+//   </article>
+// )
+const Cursos=({image,imageTwo,title,profesor,price})=>(
     <article className="card">
-    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://images.pexels.com/photos/3052361/pexels-photo-3052361.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""/>
+    <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl" id={title}>
+      <img src={image} alt={title}/>
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-      <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
-      </h3>
+        <h3 className="center">{title}</h3>
+     
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-              <img src="https://images.pexels.com/photos/1105766/pexels-photo-1105766.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+              <img src={imageTwo} alt={profesor}/>
             </div>
           </div>
-          <span className="small">Alexys Lozada</span>
+          <span className="small">{profesor}</span>
         </div>
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="#">{`$ ${price}`}</a>
       </div>
     </div>
   </article>
 )
+Cursos.prototype={
+  title: PropTypes.string,
+  image: PropTypes.string,
+  imageTwo: PropTypes.string,
+  profesor: PropTypes.string,
+  price: PropTypes.number
+}
+Cursos.defaultProps={
+  title: "No hay titulo",
+  image: "https://concepto.de/wp-content/uploads/2018/08/Londres-e1533855310803.jpg",
+  imageTwo: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Taipei_Skyline_2015.jpg",
+  profesor: "",
+  price: "--"
+}
+
 export default Cursos;
